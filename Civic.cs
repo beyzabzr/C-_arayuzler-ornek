2b00a0f7877fc15a174ab51b0e30574911be864a
beyzabzr <113631271+beyzabzr@users.noreply.github.com>
2023-12-08 20:50:41 +0300

    main

---
+++ Civic.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Net.Http.Headers;
+namespace arayuzler_ornek
+{
+    public class Civic : IOtomobil
+    {
+        public Marka HangiMarkaninAraci()
+        {
+            return Marka.Honda;
+        }
+
+        public int KacTekerlektenOlusur()
+        {
+            return 4;
+        }
+
+        public Renk StandartRengiNe()
+        {
+            return Renk.Gri;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace arayuzler_ornek
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Focus focus = new Focus();
+            Console.WriteLine(focus.HangiMarkaninAraci().ToString());
+            Console.WriteLine(focus.KacTekerlektenOlusur());
+            Console.WriteLine(focus.StandartRengiNe().ToString());
+
+            Console.WriteLine("********");
+
+            Civic civic = new Civic();
+            Console.WriteLine(civic.HangiMarkaninAraci().ToString());
+            Console.WriteLine(civic.KacTekerlektenOlusur());
+            Console.WriteLine(civic.StandartRengiNe().ToString());
+        }
+    }
+}
